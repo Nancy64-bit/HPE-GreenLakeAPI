a1a2fe792f725444b44c0086865dfaeacaee4408
HackerGwen <MissZeaffy@outlook.com>
2023-08-28 00:47:19 +0530

    intit

---
+++ src/main/java/com/hpe/greenlake/api/GreenlakeApiApplication.java
@@ -0,0 +1,13 @@
+package com.hpe.greenlake.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenlakeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenlakeApiApplication.class, args);
+	}
+
+}
